b3a26474d177ea36f7f9487fbb2930f1bc2ce0c7
komyak9 <komyak9@gmail.com>
2021-04-29 09:33:42 +0300

    saving added

--- server/src/main/java/Save.java
+++ server/src/main/java/Save.java
@@ -1,2 +1,21 @@
+import content.Worker;
+import parser.WorkerParser;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Save {
+    public void save(String fileName, CollectionManager collectionManager) throws IOException, IllegalAccessException {
+        File file = new File(fileName);
+        FileOutputStream fstream = new FileOutputStream(file, true);
+        String line = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<workers>\n";
+        byte[] buffer = line.getBytes();
+        fstream.write(buffer);
+
+        WorkerParser parser = new WorkerParser();
+        for (Worker worker : collectionManager.getWorkersList()) {
+            parser.parseObjectToXML(file, worker);
+        }
+    }
 }
